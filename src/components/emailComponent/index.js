88fe299518cbf5fa9cc53ca660d4d08c50d5ceae
Jagiella, Stanislaw <Stanislaw.Jagiella.ctr@sabre.com>
2017-04-24 09:34:21 +0200

    Make Sass file working

--- src/components/emailComponent/index.js
+++ src/components/emailComponent/index.js
@@ -23,21 +23,23 @@ class EmailComponent extends React.Component {
   render() {
     const { sendEmail } = this.props;
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-        <div>
-          <label htmlFor="firstName">First Name</label>
-          <Field name="firstName" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="lastName">Last Name</label>
-          <Field name="lastName" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <Field name="email" component="input" type="email" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <div className={styles.elthecoderEmail}>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <div>
+            <label htmlFor="firstName">First Name</label>
+            <Field name="firstName" component="input" type="text" />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name</label>
+            <Field name="lastName" component="input" type="text" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <Field name="email" component="input" type="email" />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     );
   }
 }
